4ea41eb67bbea98a4ee3184270c9f77eeed01099
Jonas Nilsson <mjc.nilsson@gmail.com>
2020-05-13 10:18:34 +0200

    Updated to 2.0.3.

--- conanfile.py
+++ conanfile.py
@@ -1,35 +1,34 @@
 import os
 from conans import ConanFile, CMake, tools
 
-
 class GraylogloggerConan(ConanFile):
     name = "graylog-logger"
-    version = "2.0.1-dm1"
-    version_number = "2.0.1"
+    version = "2.0.3-dm1"
+    version_number = "2.0.3"
     license = "BSD 2-Clause"
     url = "https://bintray.com/ess-dmsc/graylog-logger"
-    requires = ("jsonformoderncpp/3.6.1@vthiery/stable", "asio/1.13.0@bincrafters/stable", "concurrentqueue/8f7e861@ess-dmsc/stable", "fmt/6.0.0@bincrafters/stable")
+    requires = ("jsonformoderncpp/3.7.0", "asio/1.16.0", "concurrentqueue/8f7e861@ess-dmsc/stable", "fmt/6.2.0")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "virtualrunenv", "virtualbuildenv"
+    generators = ("cmake_find_package")
     description = "A simple logging library with support for pushing messages to a graylog-logger service."
-
+    
     def source(self):
         self.run("git clone https://github.com/ess-dmsc/graylog-logger.git")
         self.run("cd graylog-logger && git checkout v{}".format(self.version_number))
-    
+        
     def _configure_cmake(self):
         cmake = CMake(self, parallel=True)
 
         cmake.definitions["BUILD_EVERYTHING"] = "OFF"
+        cmake.definitions["CONAN"] = "DISABLE"
         if tools.os_info.is_macos:
             cmake.definitions["CMAKE_MACOSX_RPATH"] = "ON"
             cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = "-headerpad_max_install_names"
         cmake.configure(source_dir=self.name, build_dir=".")
         return cmake
-
+        
     def build(self):
         cmake = self._configure_cmake()
-        
         cmake.build()
 
         if tools.os_info.is_macos:
@@ -44,7 +43,6 @@ class GraylogloggerConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        
         self.copy("LICENSE.*", src="graylog-logger")
 
     def package_info(self):
