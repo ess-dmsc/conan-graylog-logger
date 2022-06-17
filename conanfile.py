83fe27a4952ec75aa4e6d1531962a214f159d54c
Afonso Mukai <afonso.mukai@ess.eu>
2022-06-17 13:59:43 +0200

    Build and run unit tests

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 class GraylogloggerConan(ConanFile):
     name = "graylog-logger"
     version_number = "2.1.4"
-    version = f"{version_number}-dm1"
+    version = f"{version_number}"
     license = "BSD 2-Clause"
     url = "https://bintray.com/ess-dmsc/graylog-logger"
     build_requires = ("gtest/1.11.0",)
@@ -31,6 +31,8 @@ class GraylogloggerConan(ConanFile):
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
+        cmake.build(target="unit_tests")
+        self.run("unit_tests/unit_tests")
 
         if tools.os_info.is_macos:
             os.system("install_name_tool -id '@rpath/libgraylog_logger.dylib' "
